52918feb93a8e2e55242b45f2bb9f7c8d8569862
Arthur Meßner <arthur.messner@gmail.com>
2026-05-09 15:25:53 +0200

    tool to create self-signed certificates

---
+++ dummy_certs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Create directory for certificates
+mkdir -p ./certs
+# Generate temporary certificate and private key
+openssl req -x509 -newkey rsa:2048 -keyout dummy.key -out dummy.crt -days 365 -nodes -subj "/CN=strava-mt.messner.click"
+# Combine both files into a single .pem file for HAProxy
+cat dummy.crt dummy.key > ./certs/messner.click.pem
+# Remove temporary files
+rm dummy.crt dummy.key
+# Print success message
+echo "Dummy certificate created at ./certs/messner.click.pem"
